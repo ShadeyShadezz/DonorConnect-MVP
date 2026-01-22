86b525a01da81921ef3d2193c08537dec2ce1d56
William <Shadowsteampunk@gmail>
2026-01-22 11:08:50 -0500

    NOW HERES WHAT MY -

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,5 +3,13 @@
 import { SessionProvider } from "next-auth/react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      basePath="/api/auth"
+      refetchInterval={5 * 60}
+      refetchOnWindowFocus={true}
+    >
+      {children}
+    </SessionProvider>
+  );
 }
